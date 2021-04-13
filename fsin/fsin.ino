0b5d7316dee7a80786c318f9444d7d018ff3f73d
xanderstruyk1 <xanderstruyk2023@u.northwestern.edu>
2021-04-13 12:47:46 -0500

    Added pinMode for each pin
    Prof Dahl gave me a map of the I/O's

--- fsin/fsin.ino
+++ fsin/fsin.ino
@@ -1,3 +1,13 @@
+/* Prof Dahl laid out an I/O map
+ * <- 0-15 SQ Waves
+ * -> 16-31 (not 13) Trig in
+ * <- 33-47 Trig latch
+ * -> 48 Trig reset
+ * <- 49 Trig OR (unlatched)
+ * <- 50 ON-Time status bit
+ * <- 51 Heartbeat SQ wave?
+ */
+
 typedef struct square_wave {
     unsigned long period;
     unsigned long phase;
@@ -14,6 +24,12 @@ unsigned long DELAY_TIME = 100; // 10 us
 unsigned long delayStart = 0; // the time the delay started
 
 void setup (void) {
+  for(int a=0;a<16;) pinMode(a++,OUTPUT); // PIN OUT for SQ waves
+  for(int a=16;a<32;) pinMode(a++,INPUT); // PIN IN for Trig in
+  for(int a=32;a<47;) pinMode(a++, OUTPUT); // PIN OUT for Trig latch
+  pinMode(48, INPUT); // PIN IN for the unlatched Trig reset
+  for(int a=49;a<52;) pinMode(a++,INPUT); // PIN OUT for Trig OR, ON-Time, Heartbeat
+  
   wave1.period = 10; //this should be going 100fps
   wave1.phase = 10;
   wave1.duty = 10;
