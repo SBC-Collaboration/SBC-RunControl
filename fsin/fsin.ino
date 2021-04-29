91a2bde4be4fc48c022d65371fcfe9bd07b736cc
xanderstruyk1 <xanderstruyk2023@u.northwestern.edu>
2021-04-29 13:14:10 -0500

    Added PORT mappings for FIFO
    Still need to map ports for single inputs and outputs to speed up more

--- fsin/fsin.ino
+++ fsin/fsin.ino
@@ -28,7 +28,7 @@ Square_Wave wave2;
 Square_Wave wave3; 
 Square_Wave wave4;
 Square_Wave wave5;
-unsigned long DELAY_TIME = 150; // 10 us
+unsigned long DELAY_TIME = 85; // 10 us
 unsigned long delayStart = 0; // the time the delay started
 unsigned long prevTime = 0;
 
@@ -39,10 +39,14 @@ void setup (void) {
   //Serial.begin(9600);
   
   for(int a=2;a<18;) pinMode(a++,OUTPUT); // PIN OUT for SQ waves
-  for(int a=18;a<34;) pinMode(a++,INPUT); // PIN IN for Trig in
-  for(int a=34;a<50;) pinMode(a++, OUTPUT); // PIN OUT for Trig latch
-  pinMode(50, INPUT); // PIN IN for Trig reset
-  for(int a=51;a<54;) pinMode(a++,OUTPUT); // PIN OUT for Trig OR, ON-Time, Heartbeat
+//  for(int a=18;a<34;) pinMode(a++,INPUT); // PIN IN for Trig in
+//  for(int a=34;a<50;) pinMode(a++, OUTPUT); // PIN OUT for Trig latch
+  DDRA = B00000000; // Fan IN PINs 22-29
+  DDRC = B00000000; // Fan IN PINs 37-30
+  DDRB = B11111111; // Fan OUT PINs 53-50, 10-13
+  DDRL = B11111111; // Fan OUT PINs 49-42
+  pinMode(38, INPUT); // PIN IN for Trig reset
+  for(int a=39;a<42;) pinMode(a++,OUTPUT); // PIN OUT for Trig OR, ON-Time, Heartbeat
   
   wave1.period = 20; //this should be going 100fps
   wave1.phase = 0;
@@ -111,8 +115,8 @@ void loop (void) {
   while ((micros() - delayStart) <= DELAY_TIME) { 
     ontime = true;
   }
-  if(ontime) digitalWrite(52, HIGH);
-  else digitalWrite(52,LOW);
+  if(ontime) digitalWrite(40, HIGH);
+  else digitalWrite(40,LOW);
   
   delayStart += DELAY_TIME; // this prevents drift in the delays
   //add hearbeat pin. high-low-high low on every delay time. 
@@ -152,30 +156,26 @@ void loop (void) {
       digitalWrite(wave->pin, HIGH); 
     }
   }}
-  
+
+  int fISum = LOW;
   
   //Or gate for the fan in
-  fISum=0;
-  for(int i=18; i<34;){
-    if (digitalRead(i++)>0) {
-      latchState = HIGH;
-      fISum = HIGH;
-      break;
-    }
+  if(PINA+PINC>0) {
+    latchState = HIGH;
+    fISum = HIGH;
+    PORTB = B11111111;
+    PORTL = B11111111;
   }
-  for(int i=34; i<50;){
-    digitalWrite(i++, latchState);
-  }
+
   
   //Trigger reset
-  if(digitalRead(50) == HIGH && latchState == HIGH){
+  if(digitalRead(38)==1 && latchState == HIGH){
     latchState = LOW;
-    for(int i=34; i<50;){
-      digitalWrite(i++, LOW);
-    }
+    PORTB = B00000000;
+    PORTL = B00000000;
   }
+  
   //Trig OR (unlatched)
-  digitalWrite(51, fISum);
-  fISum = LOW;
+  digitalWrite(39, fISum);
 
 } 
