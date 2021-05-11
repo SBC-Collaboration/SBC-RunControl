c562051835ebc78bc9d54d7b81b6fd2b15c49138
xanderstruyk1 <xanderstruyk2023@u.northwestern.edu>
2021-05-11 11:59:00 -0500

    Update fsin.ino

--- fsin/fsin.ino
+++ fsin/fsin.ino
@@ -35,12 +35,15 @@ unsigned long prevTime = 0;
 int fISum;
 int latchState = LOW;
 
+int powers[] = {2, 8,16,32,64,128,256};
+int waveNum = 0;
+
 void setup (void) {
   //Serial.begin(9600);
   
   for(int a=2;a<18;) pinMode(a++,OUTPUT); // PIN OUT for SQ waves
 
-  DDRB = B11111111; // PIN OUT for SQ waves
+  DDRH = B11111111; // PIN OUT for SQ waves
 //  for(int a=18;a<34;) pinMode(a++,INPUT); // PIN IN for Trig in
 //  for(int a=34;a<50;) pinMode(a++, OUTPUT); // PIN OUT for Trig latch
   DDRA = B00000000; // Fan IN PINs 22-29
@@ -50,9 +53,9 @@ void setup (void) {
   pinMode(38, INPUT); // PIN IN for Trig reset
   for(int a=39;a<42;) pinMode(a++,OUTPUT); // PIN OUT for Trig OR, ON-Time, Heartbeat
   
-  wave1.period = 10; //this should be going 100fps
+  wave1.period = 4; //this should be going 100fps
   wave1.phase = 0;
-  wave1.duty = 5;
+  wave1.duty = 1 ;
   wave1.polarity = true;
   wave1.counter = 0;
   wave1.pin = 2;
@@ -72,7 +75,7 @@ void setup (void) {
   wave2.period = 2; //this should be going 100fps
   wave2.phase = 0;
   wave2.duty = 1;
-  wave2.polarity = true;
+  wave2.polarity = false;
   wave2.counter = 0;
   wave2.pin = 3;
   wave2.state = 0;
@@ -125,6 +128,7 @@ void loop (void) {
   //status pin as probe on ontime to makesure that delaytime is taking the right amount of time. 
   //update_wave(&wave1);
 
+  
   //Waves:
   for(int w=0; w<5; w++){
     Square_Wave* wave = &waves[w];
@@ -134,7 +138,7 @@ void loop (void) {
     else if (wave->state==1 && wave->counter > (wave->phase + wave->duty)){
       wave->state = 2;
     }
-    else if (wave->state==2 && wave->counter> wave->period){
+    else if (wave->state==2 && wave->counter> wave->period-1){
        wave->state = 0;
        wave->counter = 0;
     }
@@ -143,25 +147,31 @@ void loop (void) {
   
     if (wave->state==1) {
       if (wave->polarity){
-        digitalWrite(wave->pin,HIGH); 
+        //digitalWrite(wave->pin,HIGH); 
+        waveNum |= powers[w];
       }
       else
       {
-        digitalWrite(wave->pin, LOW); 
+        //digitalWrite(wave->pin, LOW); 
+        waveNum &= 511-powers[w];
       }
     }
     else 
     {
        if (wave->polarity){
-        digitalWrite(wave->pin,LOW); 
+        //digitalWrite(wave->pin,LOW);
+        waveNum &= 511-powers[w];
       }
       else
       {
-        digitalWrite(wave->pin, HIGH); 
+        //digitalWrite(wave->pin, HIGH); 
+        waveNum |= powers[w];
       }
     }
   }
-
+  waveNum = (waveNum / 2)*2 + !(waveNum % 2);
+  PORTH = waveNum % 256;
+  
   int fISum = LOW;
   
   //Or gate for the fan in
