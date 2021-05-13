341366f2430dc07dcee04dcd4e7da3028421b5ea
xanderstruyk1 <xanderstruyk2023@u.northwestern.edu>
2021-05-13 15:30:03 -0500

    Update fsin.ino

--- fsin/fsin.ino
+++ fsin/fsin.ino
@@ -1,23 +1,21 @@
-/* Prof Dahl laid out an I/O map
- * <- 2-17 SQ Waves
- * -> 18-33 Trig in
- * <- 34-49 Trig latch
- * -> 50 Trig reset
- * <- 51 Trig OR (unlatched)
- * <- 52 ON-Time status bit
- * <- 53 Heartbeat SQ wave?
- * 
- * IMPORTANT: PINs 20 and 21 must be plugged in, otherwise they read HIGH by default and that would not work.
+/* I/O Map:
+ * SQ Waves (bracketed are synced): [17, 16, 6, 7, 8, 9], [5, 2, 3]
+ * FAN IN: 22-37
+ * FAN OUT: 10-13, 42-53
+ * TRIG RESET: 38
+ * TRIG OR (unlatched): 39
+ * ON-TIME: 40
+ * Heartbeat: 41
  */
 
 typedef struct square_wave {
-    unsigned long period;
-    unsigned long phase;
-    unsigned long duty;
-    bool polarity;
+    unsigned long period; // x 100us = period (s)  
+    unsigned long phase; // x 100us = offset
+    unsigned long duty; // x 100us = duration high
+    bool polarity; // start high or low
     int counter; //this is what Dr. Dahl called 'clock'
-    int pin;
-    int state;
+    int pin; 
+    int state; // internal variable for whether it is currently high or low
    } Square_Wave;
 Square_Wave waves[16];
 
@@ -28,6 +26,10 @@ Square_Wave wave2;
 Square_Wave wave3; 
 Square_Wave wave4;
 Square_Wave wave5;
+Square_Wave wave6;
+Square_Wave wave7;
+Square_Wave wave8;
+Square_Wave wave9;
 unsigned long DELAY_TIME = 100; // 10 us
 unsigned long delayStart = 0; // the time the delay started
 unsigned long prevTime = 0;
@@ -35,14 +37,15 @@ unsigned long prevTime = 0;
 int fISum;
 int latchState = LOW;
 
-int powers[] = {2, 8,16,32,64,128,256};
+int powers[] = {1,2, 8,16,32,64,128,256,512};
 int waveNum = 0;
 
 void setup (void) {
   //Serial.begin(9600);
   
-  for(int a=2;a<18;) pinMode(a++,OUTPUT); // PIN OUT for SQ waves
-
+  for(int a=2;a<18;) pinMode(a++,OUTPUT); // PIN OUT for SQ waves 
+  
+  //SQ Wave PINs in order: 17 16 6 7 8 9 5 2 3
   DDRH = B11111111; // PIN OUT for SQ waves
 //  for(int a=18;a<34;) pinMode(a++,INPUT); // PIN IN for Trig in
 //  for(int a=34;a<50;) pinMode(a++, OUTPUT); // PIN OUT for Trig latch
@@ -53,29 +56,21 @@ void setup (void) {
   pinMode(38, INPUT); // PIN IN for Trig reset
   for(int a=39;a<42;) pinMode(a++,OUTPUT); // PIN OUT for Trig OR, ON-Time, Heartbeat
   
-  wave1.period = 4; //this should be going 100fps
+  wave1.period = 100; //this should be going 100fps
   wave1.phase = 0;
-  wave1.duty = 1 ;
+  wave1.duty = 10 ;
   wave1.polarity = true;
   wave1.counter = 0;
   wave1.pin = 2;
   wave1.state = 0;
-  pinMode(wave1.pin, OUTPUT);
-  digitalWrite(wave1.pin, LOW); 
+  
   delayStart = micros(); //start delay
   delayRunning = true; // not finished yet
-  //unsigned long period;
-  //unsigned long phase;
-  //unsigned long duty;
-  //byte polarity;
-  //int counter; //this is what Dr. Dahl called 'clock'
-  //int pin;
-  //int state;
 
-  wave2.period = 2; //this should be going 100fps
+  wave2.period = 3; //this should be going 100fps
   wave2.phase = 0;
   wave2.duty = 1;
-  wave2.polarity = false;
+  wave2.polarity = true;
   wave2.counter = 0;
   wave2.pin = 3;
   wave2.state = 0;
@@ -83,7 +78,7 @@ void setup (void) {
   wave3.period = 20; //this should be going 100fps
   wave3.phase = 0;
   wave3.duty = 5;
-  wave3.polarity = false;
+  wave3.polarity = true;
   wave3.counter = 0;
   wave3.pin = 4;
   wave3.state = 0;
@@ -99,38 +94,70 @@ void setup (void) {
   wave5.period = 20; //this should be going 100fps
   wave5.phase = 0;
   wave5.duty = 5;
-  wave5.polarity = false;
+  wave5.polarity = true;
   wave5.counter = 0;
   wave5.pin = 6;
   wave5.state = 0;
   wave5.period = 20; 
+
+  wave6.period = 20; //this should be going 100fps
+  wave6.phase = 0;
+  wave6.duty = 5;
+  wave6.polarity = false;
+  wave6.counter = 0;
+  wave6.pin = 6;
+  wave6.state = 0;
+  wave6.period = 20;
+  
+  wave7.period = 20; //this should be going 100fps
+  wave7.phase = 0;
+  wave7.duty = 5;
+  wave7.polarity = true;
+  wave7.counter = 0;
+  wave7.pin = 6;
+  wave7.state = 0;
+  wave7.period = 20;
+
+  wave8.period = 20; //this should be going 100fps
+  wave8.phase = 0;
+  wave8.duty = 5;
+  wave8.polarity = false;
+  wave8.counter = 0;
+  wave8.pin = 6;
+  wave8.state = 0;
+  wave8.period = 20;
+  
+  wave9.period = 20; //this should be going 100fps
+  wave9.phase = 0;
+  wave9.duty = 5;
+  wave9.polarity = true;
+  wave9.counter = 0;
+  wave9.pin = 6;
+  wave9.state = 0;
+  wave5.period = 20;
   
   waves[0] = wave1;
   waves[1] = wave2;
   waves[2] = wave3;
   waves[3] = wave4;
   waves[4] = wave5;
+  waves[5] = wave6;
+  waves[6] = wave7;
+  waves[7] = wave8;
+  waves[8] = wave9;
   Square_Wave* wave[16];
   //for(int i=0;i<15;i++) wave[i] = &waves[i];
 } 
 
 void loop (void) {
 //  Square_Wave* wave = &wave1;
-  bool ontime = false;
-  while ((micros() - delayStart) <= DELAY_TIME) { 
-    ontime = true;
-  }
-  if(ontime) PORTG |= B00000010;
-  else digitalWrite(40,LOW);
+
   
-  delayStart += DELAY_TIME; // this prevents drift in the delays
-  //add hearbeat pin. high-low-high low on every delay time. 
-  //status pin as probe on ontime to makesure that delaytime is taking the right amount of time. 
-  //update_wave(&wave1);
+  
 
   
   //Waves:
-  for(int w=0; w<5; w++){
+  for(int w=0; w<9; w++){
     Square_Wave* wave = &waves[w];
     if (wave->state==0 && wave->counter > wave->phase){
       wave->state = 1;
@@ -169,8 +196,26 @@ void loop (void) {
       }
     }
   }
-  waveNum = (waveNum / 2)*2 + !(waveNum % 2);
-  PORTH = waveNum % 256;
+
+  //DELAY is here now
+  bool ontime = false;
+  if((micros()-delayStart)<=DELAY_TIME) ontime=true;
+  while ((micros() - delayStart) <= DELAY_TIME) { 
+  }
+  if(ontime) PORTG |= B00000010;
+  else digitalWrite(40,LOW);  
+
+  delayStart += DELAY_TIME; // this prevents drift in the delays
+  //add hearbeat pin. high-low-high low on every delay time. 
+  //status pin as probe on ontime to makesure that delaytime is taking the right amount of time. 
+  //update_wave(&wave1);
+
+  
+  PORTH = waveNum % 128;
+  PORTE = (waveNum/128) * 8;
+
+  //Heartbeat
+  PORTG = (PORTG / 2) * 2 + !(PORTG % 2);
   
   int fISum = LOW;
   
