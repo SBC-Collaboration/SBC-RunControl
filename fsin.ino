41b347fde2426f54373f2b640549eacfa14369b0
EthanARengifo <133029603+EthanARengifo@users.noreply.github.com>
2023-09-26 14:58:05 -0500

    Update fsin.ino
    Removed SquareWave function and replaced it with an include call for the file that will contain the square wave function

--- fsin.ino
+++ fsin.ino
@@ -7,18 +7,7 @@
  * ON-TIME: 40
  * Heartbeat: 41
  */
-
-typedef struct square_wave {
-    unsigned long period; // x 100us = period (s)  
-    unsigned long phase; // x 100us = offset
-    unsigned long duty; // x 100us = duration high
-    bool polarity; // start high or low
-    int counter; //this is what Dr. Dahl called 'clock'
-    int pin; 
-    int state; // internal variable for whether it is currently high or low
-   } Square_Wave;
-Square_Wave waves[16];
-
+#include <Functions.h>
 
 bool delayRunning = false; // true if still waiting for delay to finish
 Square_Wave wave1; 
