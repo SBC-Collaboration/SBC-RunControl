cfab8d1328870db27aa95295d46855f6f1126870
EthanARengifo <133029603+EthanARengifo@users.noreply.github.com>
2023-09-26 15:00:11 -0500

    Update Functions.cpp
    Adding square wave function to file

--- Functions.cpp
+++ Functions.cpp
@@ -1,1 +1,10 @@
-
+typedef struct square_wave {
+    unsigned long period; // x 100us = period (s)  
+    unsigned long phase; // x 100us = offset
+    unsigned long duty; // x 100us = duration high
+    bool polarity; // start high or low
+    int counter; //this is what Dr. Dahl called 'clock'
+    int pin; 
+    int state; // internal variable for whether it is currently high or low
+   } Square_Wave;
+Square_Wave waves[16];
