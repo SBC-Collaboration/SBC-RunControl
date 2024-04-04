de16221af284a45f446c0667ec5f81edf3e7ef8b
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2024-04-04 10:44:18 -0500

    Add more printing statements in start up script

--- init.sh
+++ init.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/sh
+echo "Starting initialization script for run control."
 
 # Set up conda environment
+echo "Setting up conda environment..."
+conda update -y -n base conda
 if conda info --envs | grep -q runcontrol; then
-  echo "runcontrol environment already exists";
+  echo "Conda environment \"runcontrol\" already exists";
 else
-  echo "runcontrol environment doesn't exist. Creating environment";
+  echo "Conda environment \"runcontrol\" doesn't exist. Creating environment";
   conda env create -y -n runcontrol --file ./dependencies/conda_rc.yml;
 fi
-
 conda activate runcontrol;
+conda update -y --all
 
 # install arduino packages
+echo "Setting up Arduino cli and libraries..."
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR=./dependencies sh
 ./dependencies/arduino-cli config init
 ./dependencies/arduino-cli lib update-index
