cfe568704654f81f37d6b212a2774011eb0914b9
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2024-04-17 23:37:44 -0500

    Add config boxes for trigger arduino input pins, and a custom sppinbox widget whout wheel events

--- ui/custom_widgets.py
+++ ui/custom_widgets.py
@@ -1,11 +1,15 @@
-from PySide6.QtWidgets import QTableWidget
+from PySide6 import QtWidgets
 
 
 # Overriding original table widget so that drag and drop will swap the two values, instead of erasing the value at destination
-class SwappingTableWidget(QTableWidget):
+class SwappingTableWidget(QtWidgets.QTableWidget):
     def dropEvent(self, dropEvent):
         item_src = self.selectedItems()[0]
         item_dest = self.itemAt(dropEvent.pos())
         src_value = item_src.text()
         item_src.setText(item_dest.text())
         item_dest.setText(src_value)
+
+class NoWheelSpinBox(QtWidgets.QSpinBox):
+    def wheelEvent(self, event):
+        event.ignore()
