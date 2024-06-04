f1edb7a1a7b9d205410669f81d18efece8a9f559
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2024-06-04 13:12:51 -0500

    Add some code for CAEN module

--- src/caen.py
+++ src/caen.py
@@ -4,4 +4,19 @@ import logging
 from PySide6.QtCore import QTimer, QObject, Slot, Signal
 from collections import namedtuple
 import copy
-import random
+
+
+class Caen(QObject):
+    caen_started = Signal(str)
+    caen_stopped = Signal(str)
+
+    def __init__(self, mainwindow, cam_name):
+        super().__init__()
+        self.main = mainwindow
+        self.config = mainwindow.config_class.config["scint"]["caen"]
+        self.gp_config = {
+            "g0": mainwindow.config_class.config["scint"]["caen_g0"],
+            "g1": mainwindow.config_class.config["scint"]["caen_g1"],
+            "g2": mainwindow.config_class.config["scint"]["caen_g2"],
+            "g3": mainwindow.config_class.config["scint"]["caen_g3"]
+        }
