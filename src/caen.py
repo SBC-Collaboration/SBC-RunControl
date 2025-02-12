5b35ef21e6c2eaa033f34de2f051bd500564a299
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2025-02-12 00:00:47 -0600

    Start implementing CAEN module

--- src/caen.py
+++ src/caen.py
@@ -6,16 +6,20 @@ from collections import namedtuple
 import copy
 import json
 import red_caen
+from sbcbinaryformat import Writer
 
 class Caen(QObject):
     caen_started = Signal(str)
     caen_stopped = Signal(str)
+    event_started = Signal(str)
+    event_stopped = Signal(str)
 
     def __init__(self, mainwindow):
         super().__init__()
         self.main = mainwindow
         self.logger = logging.getLogger("rc")
         self.scint_config = self.main.config_class.config["scint"]
+        self.caen = None
 
         self.timer = QTimer(self)
         self.timer.setInterval(100)
@@ -26,195 +30,102 @@ class Caen(QObject):
         self.timer.start()
         self.logger.debug(f"Caen module initialized in {QThread.currentThread().objectName()}.")
 
+
     @Slot()
     def periodic_task(self):
-        print(f"buffer: {caen.GetEventsInBuffer()}, retrieved: {caen.GetNumberOfEvents()}")
-        caen.RetrieveDataUntilNEvents(500)
-        caen.DecodeEvents()
-        for i in range(0,500):
-            data_array_temp = caen.GetWaveform(i)
-            data_array_temp=data_array_temp.tolist()
-            del data_array_temp[32:64]
-            counter=counter + 1
-            event = caen.GetEvent(i)
-            eventsize_array.append(event.get_info().EventSize)
-            boardid_array.append(event.get_info().BoardId)
-            pattern_array.append(event.get_info().Pattern)
-            channelmask_array.append(event.get_info().ChannelMask)
-            eventcounter_array.append(event.get_info().EventCounter)
-            triggertimetag_array.append(event.get_info().TriggerTimeTag)
-            data_array.append(data_array_temp)
-        if counter >= 1000: 
-            data_array=np.array(data_array)
-            eventsize_array=np.array(eventsize_array)
-            boardid_array=np.array(boardid_array)
-            pattern_array=np.array(pattern_array)
-            channelmask_array=np.array(channelmask_array)
-            eventcounter_array=np.array(eventcounter_array)
-            triggertimetag_array=np.array(triggertimetag_array)
-            data = {
-                'Event Size': eventsize_array,
-                'Board Id':boardid_array,
-                'Pattern': pattern_array,
-                'Channel Mask':channelmask_array,
-                'Event Counter':eventcounter_array,
-                'Trigger Time Tag':triggertimetag_array,
-                'data_array': data_array}
-            caen_writer.write(data)
-            eventsize_array=[]
-            boardid_array=[]
-            pattern_array=[]
-            channelmask_array=[]
-            eventcounter_array=[]
-            triggertimetag_array=[]
-            data_array_temp=[]
-            data_array=[]
-            counter = 0
+        pass
+
+    def set_config(self):
+        self.config = self.main.config_class.run_config["scint"]["caen"]
+
+        # set global config from dict to class attributes
+        self.global_config = red_caen.CAENGlobalConfig()
+        self.global_config.MaxEventsPerRead = self.config["evs_per_read"]
+        self.global_config.RecordLength = self.config["rec_length"]
+        self.global_config.PostTriggerPorcentage = self.config["post_trig"]
+        self.global_config.EXTTriggerMode = getattr(red_caen.CAEN_DGTZ_TriggerMode, self.config["ext_trig"])
+        self.global_config.SWTriggerMode = getattr(red_caen.CAEN_DGTZ_TriggerMode, self.config["sw_trig"])
+        self.global_config.CHTriggerMode = getattr(red_caen.CAEN_DGTZ_TriggerMode, self.config["ch_trig"])
+        self.global_config.TriggerPolarity = getattr(red_caen.CAEN_DGTZ_TriggerPolarity, self.config["polarity"])
+        self.global_config.IOLevel = getattr(red_caen.CAEN_DGTZ_IOLevel, self.config["io_level"])
+        self.global_config.TrgInAsGate = self.config["trig_in_as_gate"]
+        self.global_config.TriggerOverlappingEn = self.config["overlap_en"]
+        self.global_config.MemoryFullMode = self.config["memory_full"]
+        self.global_config.DecimationFactor = self.config["decimation"]
+        self.global_config.MajorityLevel = self.config["majority_level"]
+        self.global_config.MajorityWindow = self.config["majority_window"]
+
+        self.group_configs = [red_caen.CAENGroupConfig() for _ in range(8)]
+        for i in range(4):
+            group_config = self.main.config_class.run_config["scint"][f"caen_g{i}"]
+            acq_mask = red_caen.ChannelMask()
+            trg_mask = red_caen.ChannelMask()
+            for ch in range(8):
+                acq_mask[ch] = group_config["acq_mask"][ch]
+                trg_mask[ch] = group_config["trg_mask"][ch]
+            self.group_configs[i].AcquisitionMask = acq_mask
+            self.group_configs[i].TriggerMask = trg_mask
+            self.group_configs[i].Enabled = group_config["enabled"]
+            self.group_configs[i].DCOffset = group_config["offset"]
+            self.group_configs[i].DCCorrections = group_config["ch_offset"]
+            self.group_configs[i].DCRange = 0 if group_config["range"]=="2Vpp" else 1 # 0: 2Vpp, 1: 0.5Vpp
+            self.group_configs[i].TriggerThreshold = group_config["threshold"]
+        
+        self.caen.Setup(self.global_config, self.group_configs)
+
+        self.buffer = []
 
     @Slot()
     def start_run(self):
-        model_constants = red_caen.GetCAENDigitizerModelConstants()
-        caen = red_caen.CAEN(
+        self.config = self.main.config_class.run_config["scint"]["caen"]
+
+        self.caen = red_caen.CAEN(
             red_caen.iostream_wrapper(), 
-            red_caen.CAENDigitizerModel.DT5740D, 
-            red_caen.CAENConnectionType.USB,
-            0,
+            getattr(red_caen.CAENDigitizerModel, self.config["model"]),
+            getattr(red_caen.CAENConnectionType, self.config["connection"]),
+            self.config["link"],
             0,
             0)
-        if caen.IsConnected():
-            print("CAEN connection successful.\n")
+        if self.caen.IsConnected():
+            self.logger.info("CAEN connection successful.\n")
         else:
-            print("CAEN connection not successful.\n")
+            self.logger.error("CAEN connection failed.\n")
+            raise ConnectionError("CAEN connection failed.\n")
 
+        self.set_config()
         self.run_started.emit("caen")
-
 
     @Slot()
     def start_event(self):
-        file_name = "caen.sbc.bin"
+        # Check connection status
+        if not self.caen or not self.caen.IsConnected():
+            self.logger.error("CAEN not connected.")
+        
+        self.buffer = [] # create a list for data dictionaries
+        # create writer
+        self.writer = Writer(
+            
+        )
 
-        column_names = ["Event Size","Board Id","Pattern","Channel Mask","Event Counter","Trigger Time Tag","data_array"]
-        data_types = ['u4','u4','u4','u4','u4','u4','i8']
-        sizes = [[1],[1],[1],[1],[1],[1],[32, 180]]
-        caen_writer = Writer(file_name, column_names, data_types, sizes)
+        self.caen.EnableAcquisition()
 
-        eventsize_array=[]
-        boardid_array=[]
-        pattern_array=[]
-        channelmask_array=[]
-        eventcounter_array=[]
-        triggertimetag_array=[]
-        data_array_temp=[]
-        data_array=[]
-
-
-
-        # global config
-        global_config = red_caen.CAENGlobalConfig()
-        global_config.MaxEventsPerRead = 500
-        global_config.RecordLength = 180
-        global_config.PostTriggerPorcentage = 50
-        global_config.EXTTriggerMode = red_caen.CAEN_DGTZ_TriggerMode.ACQ_ONLY
-        global_config.SWTriggerMode = red_caen.CAEN_DGTZ_TriggerMode.ACQ_ONLY
-        global_config.CHTriggerMode = red_caen.CAEN_DGTZ_TriggerMode.ACQ_ONLY
-        global_config.TriggerPolarity = red_caen.CAEN_DGTZ_TriggerPolarity.Falling
-        global_config.IOLevel = red_caen.CAEN_DGTZ_IOLevel.TTL
-        global_config.EXTAsGate = False
-        global_config.TriggerOverlappingEn = False
-        global_config.MemoryFullModeSelection = True
-        global_config.DecimationFactor = 0
-        global_config.MajorityLevel = 0
-        global_config.MajorityCoincidenceWindow = 0
-
-        # group config (need length 8 for the wrapper, but only first 4 is used)
-        group_configs = [red_caen.CAENGroupConfig() for i in range(8)]
-        for i in range(8):
-            acq_mask = red_caen.ChannelsMask()
-            trg_mask = red_caen.ChannelsMask()
-            for ch in range(8):
-                acq_mask[ch] = True
-                trg_mask[ch] = True
-        #    acq_mask[0] = False # False meas data won't be recorded from these channels.
-        #    print("acq_mask:",acq_mask)
-            group_configs[i].Enabled = True
-            group_configs[i].DCOffset = 0x8000
-            group_configs[i].DCCorrections = [0,0,0,0,0,0,0,0]
-            group_configs[i].DCRange = 0
-            group_configs[i].TriggerThreshold = 0x800
-            group_configs[i].AcquisitionMask = acq_mask
-            group_configs[i].TriggerMask = trg_mask
-
-        # set config
-        caen.Setup(global_config, group_configs)
-
-        global_back = caen.GetGlobalConfiguration()
-        print(global_back)
-
-
-        groups_back = caen.GetGroupConfigurations()
-
-        caen.EnableAcquisition()
-        counter = 0
-        self.caen_started.emit("caen")
+        self.event_started.emit("caen")
 
     @Slot()
     def stop_event(self):
-        print(f"buffer_after_while_loop: {caen.GetEventsInBuffer()}, retrieved_after_while_loop: {caen.GetNumberOfEvents()}") #BM
-        caen.RetrieveDataUntilNEvents(caen.GetEventsInBuffer()) #BM
-        caen.DecodeEvents()   #BM
-        caen.DisableAcquisition()
+        self.caen.DisableAcquisition()
+        # retrieve more data
+        self.caen.ClearData()
 
-        print(f"buffer_final: {caen.GetEventsInBuffer()}, retrieved_final: {caen.GetNumberOfEvents()}") #BM
-
-        data_array_temp=[]
-        eventsize_array=[]
-        boardid_array=[]
-        pattern_array=[]
-        channelmask_array=[]
-        eventcounter_array=[]
-        triggertimetag_array=[]
-        data_array=[]   
-        for i in range(0,caen.GetNumberOfEvents()):
-            data_array_temp = caen.GetWaveform(i)
-            data_array_temp=data_array_temp.tolist()
-            del data_array_temp[32:64]
-            event = caen.GetEvent(i)
-            eventsize_array.append(event.get_info().EventSize)
-            boardid_array.append(event.get_info().BoardId)
-            pattern_array.append(event.get_info().Pattern)
-            channelmask_array.append(event.get_info().ChannelMask)
-            eventcounter_array.append(event.get_info().EventCounter)
-            triggertimetag_array.append(event.get_info().TriggerTimeTag)
-            data_array.append(data_array_temp)
-
-        data_array=np.array(data_array)
-        eventsize_array=np.array(eventsize_array)
-        boardid_array=np.array(boardid_array)
-        pattern_array=np.array(pattern_array)
-        channelmask_array=np.array(channelmask_array)
-        eventcounter_array=np.array(eventcounter_array)
-        triggertimetag_array=np.array(triggertimetag_array)
-        data = {
-            'Event Size': eventsize_array,
-            'Board Id':boardid_array,
-            'Pattern': pattern_array,
-            'Channel Mask':channelmask_array,
-            'Event Counter':eventcounter_array,
-            'Trigger Time Tag':triggertimetag_array,
-            'data_array': data_array}
-        caen_writer.write(data)
-
-        caen.ClearData()
+        # write data to file
+        for b in self.buffer:
+            self.writer.write(b)
+        
+        self.event_stopped.emit("caen")
 
     @Slot()
     def stop_run(self):
-        #stop CAEN connection
-        del data_array_temp
-        del ventsize_array
-        del boardid_array
-        del pattern_array
-        del channelmask_array
-        del eventcounter_array
-        del triggertimetag_array
-        del data_array
-
+        del self.caen
+        del self.buffer
+        del self.global_config
+        del self.group_configs
