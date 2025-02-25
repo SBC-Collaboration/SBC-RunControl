e6c1c085acda6bdb5f80cfb6491021341da2a1d6
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2025-02-25 10:55:07 -0600

    Now acoustics module can be disabled, and also writes the ini file

--- src/acoustics.py
+++ src/acoustics.py
@@ -10,6 +10,7 @@ import src.global_variable  as gv #BM
 import subprocess
 import signal
 import time
+from configparser import ConfigParser
 
 
 class Acoustics(QObject):
@@ -20,7 +21,7 @@ class Acoustics(QObject):
         super().__init__()
         self.main = mainwindow
         self.logger = logging.getLogger("rc")
-        self.acous_config = self.main.config_class.config["acous"]
+        self.config = self.main.config_class.config["acous"]
 
         self.timer = QTimer(self)
         self.timer.setInterval(100)
@@ -35,10 +36,86 @@ class Acoustics(QObject):
     def periodic_task(self):
         pass
 
+    def save_config(self):
+        config_data = {
+            "Acquisition": {
+                "Mode": "Octal",
+                "SampleRate": "1000000",  # 1 MHz
+                "Depth": "25000000",      # e.g. for eight channels
+                "SegmentSize": "25000000",
+                "SegmentCount": "1",
+                "TriggerHoldOff": "0",
+                "TriggerTimeout": "0",    # 10 ms would be '100000' etc.
+                "TimeStampMode": "Free",
+                "TimeStampClock": "Fixed"
+            },
+            "Channel1": {
+                "Range": "2000",
+                "Coupling": "DC",
+                "Impedance": "50"
+            },
+            "Channel2": {
+                "Range": "2000",
+                "Coupling": "DC",
+                "Impedance": "50"
+            },
+            "Channel3": {
+                "Range": "2000",
+                "Coupling": "DC",
+                "Impedance": "50"
+            },
+            "Channel4": {
+                "Range": "2000",
+                "Coupling": "DC",
+                "Impedance": "50"
+            },
+            "Channel5": {
+                "Range": "2000",
+                "Coupling": "DC",
+                "Impedance": "50"
+            },
+            "Channel6": {
+                "Range": "2000",
+                "Coupling": "DC",
+                "Impedance": "50"
+            },
+            "Channel7": {
+                "Range": "2000",
+                "Coupling": "DC",
+                "Impedance": "50"
+            },
+            "Channel8": {
+                "Range": "2000",
+                "Coupling": "DC",
+                "Impedance": "50"
+            },
+            "Trigger1": {
+                "Condition": "Rising",
+                "Level": "10",
+                "Source": "2"
+            },
+            "Application": {
+                "StartPosition": "0",
+                "TransferLength": "25000000",
+                "SegmentStart": "1",
+                "SegmentCount": "1",
+                "SaveFileName": "/home/sbc/packages/gati-linux-driver/Sdk/SBC-Piezo-Base-Code/test/Acquire-",
+                "SaveFileFormat": "TYPE_DEC"
+            }
+        }
+        parser = ConfigParser()
+        parser.read_dict(config_data)
+        with open("SBCAcquisition.ini", "w") as f:
+            parser.write(f)
+
     @Slot(str)                                                                      
     def start_event(self,module):
 
         self.config = self.main.config_class.run_config[module]
+        if not self.config["enabled"]:
+            self.event_stopped.emit(f"disabled-acous")
+            return
+        self.save_config()
         
         self.logger.info(f"Acoustic data acquisition starting.")                    
         process = subprocess.Popen(['python', '/home/sbc/packages/gati-linux-driver/Sdk/SBC-Piezo-Base-Code/sbc_reader_dll_wrapper.py'])             
