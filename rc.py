1236af92218b11ecfdba3bd9795ce90a6d337a6f
Zhiheng Sheng <25388025+shengzhiheng@users.noreply.github.com>
2025-07-02 00:19:36 -0500

    Modularize CAEN and acous plotting into their own classes (still main thread).

--- rc.py
+++ rc.py
@@ -18,6 +18,7 @@ from src.sipm_amp import SiPMAmp
 from src.sql import SQL
 from src.niusb import NIUSB
 from src.writer import Writer
+from src.visualization import CAENPlotManager, AcousPlotManager
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from enum import Enum
@@ -96,6 +97,12 @@ class MainWindow(QMainWindow):
         self.file_handler.setFormatter(self.log_formatter)
         self.logger.addHandler(self.file_handler)
 
+        # visualization setup
+        self.caen_plot_mgr = CAENPlotManager(
+            self.ui.caen_plot_container, self.config_class.config)
+        self.acous_plot_mgr = AcousPlotManager(
+            self.ui.acous_plot, self.config_class.config)
+
         # define four run states
         self.run_states = Enum(
             "State",
@@ -131,8 +138,8 @@ class MainWindow(QMainWindow):
         self.stopping_event_ready = []
         self.set_up_workers()
 
-        self.setup_caen_plot()
-        self.setup_acous_plot()
+        self.caen_plot_mgr.setup_plot()
+        self.acous_plot_mgr.setup_plot()
 
         # timer for event loop
         self.timer = QTimer()
@@ -191,7 +198,7 @@ class MainWindow(QMainWindow):
         self.caen_worker.run_stopped.connect(self.stopping_run_wait)
         self.caen_worker.event_started.connect(self.starting_event_wait)
         self.caen_worker.event_stopped.connect(self.stopping_event_wait)
-        self.caen_worker.data_retrieved.connect(self.update_caen_plot)
+        self.caen_worker.data_retrieved.connect(self.caen_plot_mgr.update_plot)
         self.caen_thread = QThread()
         self.caen_thread.setObjectName("caen_thread")
         self.caen_worker.moveToThread(self.caen_thread)
@@ -206,7 +213,7 @@ class MainWindow(QMainWindow):
         self.acous_worker = Acoustics(self)
         self.acous_worker.event_started.connect(self.starting_event_wait)
         self.acous_worker.event_stopped.connect(self.stopping_event_wait)
-        self.acous_worker.event_stopped.connect(self.update_acous_plot)
+        self.acous_worker.event_stopped.connect(self.acous_plot_mgr.update_plot)
         self.acous_thread = QThread()
         self.acous_thread.setObjectName("acous_thread")
         self.acous_worker.moveToThread(self.acous_thread)
@@ -793,149 +800,6 @@ class MainWindow(QMainWindow):
             else self.config_class.config["general"]["data_dir"]
         subprocess.Popen(["nemo", path])
         self.logger.debug(f"Opened data folder: {path}")
-
-    def setup_caen_plot(self):
-        """
-        Set up the CAEN plot widgets. It will create the plot and text items for each plot, and set up basic properties.
-        """
-        # plot and text items for each plot
-        self.caen_plots = []
-        self.caen_curves = []
-        container = self.ui.caen_plot_container
-        container.ci.layout.setSpacing(0)
-        container.clear()
-        container.setBackground("w")
-        y_label = container.addLabel("Amplitude (ADC)", 
-                    color='k', row=0, col=0, angle=-90, rowspan=4, anchor=(0,0.5))
-        self.caen_event_label = pg.TextItem(text="Event: 0", color='k', anchor=(0, 0))
-        self.caen_event_label.setParentItem(y_label)
-        self.caen_event_label.setPos(0, 1)
-        container.ci.layout.setColumnFixedWidth(0, 8)
-        container.ci.layout.setColumnStretchFactor(1, 4)
-        container.ci.layout.setColumnFixedWidth(2, 10)
-        container.ci.layout.setColumnFixedWidth(3, 10)
-        # self.legend_layout = container.addLayout(row=0, col=2, rowspan=4)
-        # one pen for each channel in a group
-        self.caen_pen_colors = ["green", "red", "blue", "orange", "violet", "brown", "pink", "gray"]
-        self.caen_pens = [pg.mkPen(color=c, width=2) for c in self.caen_pen_colors]
-
-        for i in range(4):
-            p = container.addPlot(row=i, col=1)
-            self.caen_plots.append(p)
-            p.showGrid(x=True, y=True)
-            p.getViewBox().setBackgroundColor("w")
-            p.enableAutoRange('xy', True)
-            border_pen = pg.mkPen(color='black', width=2)
-            p.getViewBox().border = border_pen
-            legend = p.addLegend()
-            legend.scene().removeItem(legend)
-            legend_layout = container.addLayout(row=(i//2)*2, col=2+(i%2), rowspan=2)
-            legend_layout.layout.setSpacing(0)
-            legend_layout.addItem(legend, 0, 0)
-
-            # Do different things for each subplot
-            if i == 0:
-                p.setTitle(f"CAEN Waveforms", color='k')
-                axis = p.getAxis('bottom')
-                axis.setStyle(tickLength=0, showValues=False)
-            elif i in [1, 2]:
-                p.setXLink(self.caen_plots[0])
-                axis = p.getAxis('bottom')
-                axis.setStyle(tickLength=0, showValues=False)
-            elif i == 3:
-                p.setXLink(self.caen_plots[0])
-                p.setLabel('bottom', "Time (us)", color='k')
-            else:
-                raise ValueError("Invalid CAEN plot index")
-
-            # create curves for each channel
-            curves = []
-            for j in range(8):
-                c = p.plot(x=[], y=[], pen=self.caen_pens[j], name=f"Ch{i*8+j}")
-                curves.append(c)
-            self.caen_curves.append(curves)
-
-
-    @Slot(dict)
-    def update_caen_plot(self, data):
-        """
-        Update the CAEN plot widgets every time data is retrieved
-        """
-        def get_ch_index(mask, ch):
-            """
-            Get the index of the channel in list using mask
-            Count number of 1s in all bits lower than the desired bit
-            """
-            lower_mask = (1 << ch) - 1
-            return bin(mask & lower_mask).count("1")
-
-        caen_configs = self.config_class.config["caen"]
-        self.caen_event_label.setText(f"Event: {data['EventCounter'][-1]}")
-
-        for i in range(len(self.caen_plots)):
-            curves = self.caen_curves[i]
-            # frequency is 62.5MHz
-            time_axis = [i*0.016 for i in range(data["Waveforms"].shape[2])]
-            for j in range(8):
-                if not caen_configs[f"group{i}"]["enabled"] or \
-                  not caen_configs[f"group{i}"]["acq_mask"][j] or \
-                  not caen_configs[f"group{i}"]["plot_mask"][j]:
-                    curves[j].setData(x=[], y=[])
-                else:
-                    ind = get_ch_index(data["AcquisitionMask"], 8*i+j)
-                    wfs = data['Waveforms'][-1][ind]
-                    curves[j].setData(x=time_axis, y=wfs)
-
-    def setup_acous_plot(self):
-        """
-        Set up the Acous plot widgets. It will create the plot and text items for each plot, and set up basic properties.
-        """
-        # plot and text items for each plot
-        self.acous_plot = self.ui.acous_plot
-        self.acous_curves = []
-        self.acous_text = pg.TextItem(text="Event: 0", color='k', anchor=(0,0))
-
-        # one pen for each channel in a group
-        self.acous_pens = [pg.mkPen(color=c, width=2) for c in ["green", "red", "blue", "orange","violet","brown","pink","gray"]] 
-
-        # setup some basic plot properties
-        self.acous_plot.setBackground("w")
-        self.acous_plot.showGrid(x=True, y=True)
-        self.acous_plot.setTitle(f"Acous Waveforms", color='k')
-        self.acous_plot.setLabel('left', "Amplitude (ADC)", color='k')
-        self.acous_plot.setLabel('bottom', "Time (us)", color='k')
-        self.acous_plot.addLegend()
-
-        # setup text counter
-        self.acous_text.setParentItem(self.acous_plot.getViewBox())
-        self.acous_text.setPos(0, 1)
-
-        # create curves for each channel
-        for i in range(8):
-            c = self.acous_plot.plot(x=[], y=[], pen=self.acous_pens[i], name=f"Ch{i+1}")
-            self.acous_curves.append(c)
-
-    @Slot(dict)
-    def update_acous_plot(self):
-        """
-        Update the acous plot widgets every time data is retrieved
-        """
-        acous_configs = self.config_class.config["acous"]
-        if not acous_configs["enabled"]:
-            return
-        
-        try:
-            data = Streamer(os.path.join(self.current_path, "acoustics.sbc")).to_dict()
-        except FileNotFoundError:
-            self.logger.warning("Acoustics data file not found.")
-            return
-        sample_rate = self.acous_worker.sample_rate_conversion[acous_configs["sample_rate"]]/1e6
-        time_axis = [i/sample_rate for i in range(data["Waveforms"].shape[2])]
-        for i in range(8):
-            if not acous_configs[f"ch{i+1}"]["plot"]:
-                self.acous_curves[i].setData(x=[], y=[])
-            else:
-                self.acous_curves[i].setData(x=time_axis, y=data['Waveforms'][-1][i])
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
---
+++ src/visualization.py
@@ -0,0 +1,157 @@
+import pyqtgraph as pg
+from PySide6.QtCore import QObject
+from sbcbinaryformat import Streamer
+import os
+
+class CAENPlotManager(QObject):
+    def __init__(self, plot_container, config):
+        super().__init__()
+        self.container = plot_container
+        self.plots = []
+        self.curves = []
+        self.event_label = None
+        # one pen for each channel in a group
+        self.pen_colors = ["green", "red", "blue", "orange", "violet", "brown", "pink", "gray"]
+        self.pens = [pg.mkPen(color=c, width=2) for c in self.pen_colors]
+        self.main_config = config
+        
+    def setup_plot(self):
+        """
+        Set up the CAEN plot widgets. It will create the plot and text items for each plot, and set up basic properties.
+        """
+        # plot and text items for each plot
+        self.container.ci.layout.setSpacing(0)
+        self.container.clear()
+        self.container.setBackground("w")
+        y_label = self.container.addLabel("Amplitude (ADC)", 
+                    color='k', row=0, col=0, angle=-90, rowspan=4, anchor=(0,0.5))
+        self.event_label = pg.TextItem(text="Event: 0", color='k', anchor=(0, 0))
+        self.event_label.setParentItem(y_label)
+        self.event_label.setPos(0, 1)
+        self.container.ci.layout.setColumnFixedWidth(0, 8)
+        self.container.ci.layout.setColumnStretchFactor(1, 4)
+        self.container.ci.layout.setColumnFixedWidth(2, 10)
+        self.container.ci.layout.setColumnFixedWidth(3, 10)
+        
+        for i in range(4):
+            p = self.container.addPlot(row=i, col=1)
+            self.plots.append(p)
+            p.showGrid(x=True, y=True)
+            p.getViewBox().setBackgroundColor("w")
+            p.enableAutoRange('xy', True)
+            border_pen = pg.mkPen(color='black', width=2)
+            p.getViewBox().border = border_pen
+            legend = p.addLegend()
+            legend.scene().removeItem(legend)
+            legend_layout = self.container.addLayout(row=(i//2)*2, col=2+(i%2), rowspan=2)
+            legend_layout.layout.setSpacing(0)
+            legend_layout.addItem(legend, 0, 0)
+
+            # Do different things for each subplot
+            if i == 0:
+                p.setTitle(f"CAEN Waveforms", color='k')
+                axis = p.getAxis('bottom')
+                axis.setStyle(tickLength=0, showValues=False)
+            elif i in [1, 2]:
+                p.setXLink(self.plots[0])
+                axis = p.getAxis('bottom')
+                axis.setStyle(tickLength=0, showValues=False)
+            elif i == 3:
+                p.setXLink(self.plots[0])
+                p.setLabel('bottom', "Time (us)", color='k')
+            else:
+                raise ValueError("Invalid CAEN plot index")
+
+            # create curves for each channel
+            curves = []
+            for j in range(8):
+                c = p.plot(x=[], y=[], pen=self.pens[j], name=f"Ch{i*8+j}")
+                curves.append(c)
+            self.curves.append(curves)
+
+
+    def update_plot(self, data):
+        """
+        Update the CAEN plot widgets every time data is retrieved
+        """
+        self.event_label.setText(f"Event: {data['EventCounter'][-1]}")
+        configs = self.main_config["caen"]
+
+        for i in range(len(self.plots)):
+            curves = self.curves[i]
+            # frequency is 62.5MHz
+            time_axis = [i*0.016 for i in range(data["Waveforms"].shape[2])]
+            for j in range(8):
+                if not configs[f"group{i}"]["enabled"] or \
+                    not configs[f"group{i}"]["acq_mask"][j] or \
+                    not configs[f"group{i}"]["plot_mask"][j]:
+                    curves[j].setData(x=[], y=[])
+                else:
+                    ind = self._get_ch_index(data["AcquisitionMask"], 8*i+j)
+                    wfs = data['Waveforms'][-1][ind]
+                    curves[j].setData(x=time_axis, y=wfs)
+        
+
+    @staticmethod
+    def _get_ch_index(mask, ch):
+        """
+        Get the index of the channel in list using mask
+        Count number of 1s in all bits lower than the desired bit
+        """
+        lower_mask = (1 << ch) - 1
+        return bin(mask & lower_mask).count("1")
+
+
+class AcousPlotManager(QObject):
+    def __init__(self, plot, config):
+        super().__init__()
+        self.plot = plot
+        self.curves = []
+        self.text = None
+        self.pens = []
+        self.main_config = config
+
+    def setup_plot(self):
+        """
+        Set up the Acous plot widgets. It will create the plot and text items for each plot, and set up basic properties.
+        """
+        self.text = pg.TextItem("", anchor=(0, 1), color='k')
+        self.pens = [pg.mkPen(color=c, width=2) for c in ["green", "red", "blue", "orange","violet","brown","pink","gray"]] 
+
+        # setup some basic plot properties
+        self.plot.setBackground("w")
+        self.plot.showGrid(x=True, y=True)
+        self.plot.setTitle(f"Acous Waveforms", color='k')
+        self.plot.setLabel('left', "Amplitude (ADC)", color='k')
+        self.plot.setLabel('bottom', "Time (us)", color='k')
+        self.plot.addLegend()
+
+        # setup text counter
+        self.text.setParentItem(self.plot.getViewBox())
+        self.text.setPos(0, 1)
+
+        # create curves for each channel
+        for i in range(8):
+            c = self.plot.plot(x=[], y=[], pen=self.pens[i], name=f"Ch{i+1}")
+            self.curves.append(c)
+
+    def update_plot(self):
+        """
+        Update the acous plot widgets every time data is retrieved
+        """
+        acous_configs = self.config["acous"]
+        if not acous_configs["enabled"]:
+            return
+        
+        try:
+            data = Streamer(os.path.join(self.current_path, "acoustics.sbc")).to_dict()
+        except FileNotFoundError:
+            self.logger.warning("Acoustics data file not found.")
+            return
+        sample_rate = self.acous_worker.sample_rate_conversion[acous_configs["sample_rate"]]/1e6
+        time_axis = [i/sample_rate for i in range(data["Waveforms"].shape[2])]
+        for i in range(8):
+            if not acous_configs[f"ch{i+1}"]["plot"]:
+                self.curves[i].setData(x=[], y=[])
+            else:
+                self.curves[i].setData(x=time_axis, y=data['Waveforms'][-1][i])
